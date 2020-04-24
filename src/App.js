6502aaabb36e53d97855518d50ff976c1764c4f4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2020-04-24 23:58:36 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
+      <Resume/>
     </div>
   );
 }
