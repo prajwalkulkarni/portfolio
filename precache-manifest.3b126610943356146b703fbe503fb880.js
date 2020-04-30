ff52de581e966f2bd7a19bf15c9804ee4943ca7c
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2020-05-01 00:09:31 +0530

    Updates

---
+++ precache-manifest.3b126610943356146b703fbe503fb880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8289f5ff1fe45edd877da40943360cd7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "39e8714ca532817dcbdf",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "871b2a98cee7a6abe712",
+    "url": "/portfolio/static/js/2.56ac96b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.56ac96b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e8714ca532817dcbdf",
+    "url": "/portfolio/static/js/main.e8a7f290.chunk.js"
+  },
+  {
+    "revision": "a3f62719c08a27d64766",
+    "url": "/portfolio/static/js/runtime-main.e387a7d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
